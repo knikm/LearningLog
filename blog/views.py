9319d3e426ba0ae07b1e6ec7c0b87fd0bbe084e5
Kanika <kanikaarora356@gmail.com>
2020-02-11 20:12:53 +0530

    Completed Databases and migrations

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts =[ 
-    {
-    'author' : 'Kay',
-    'title'  : 'Blog Post 1',
-    'content': 'First Post content',
-    'date_posted' : '14 January 2020'
-    },
-    {
-    'author' : 'Sam',
-    'title'  : 'Blog Post 2',
-    'content': 'Second Post content',
-    'date_posted' : '15 January 2020'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
